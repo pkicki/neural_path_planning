0e57cc0cafda7f1b645cc15684957cff958c14b3
Kicajowyfreestyle <piotr.m.kicki@gmail.com>
2021-11-04 08:19:05 +0100

    Move np concatenate to tf concat

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 
 class Car:
     ## Kia Rio III hatchback dimensions
@@ -13,7 +14,7 @@ class Car:
     back_left = np.array([-rear_axle_to_back, W / 2], dtype=np.float32)[np.newaxis]
     back_right = np.array([-rear_axle_to_back, -W / 2], dtype=np.float32)[np.newaxis]
 
-    crucial_points = np.concatenate([np.zeros_like(front_left), front_left, front_right, back_right, back_left], axis=0)
+    crucial_points = tf.concat([np.zeros_like(front_left), front_left, front_right, back_right, back_left], axis=0)
 
     tw = np.linspace(0., 1., int(W / 0.2))[:, np.newaxis]
     tl = np.linspace(0., 1., int((rear_axle_to_front + rear_axle_to_back) / 0.2))[:, np.newaxis]
@@ -21,4 +22,4 @@ class Car:
     back = back_left * tw + (1 - tw) * back_right
     left = front_left * tl + (1 - tl) * back_left
     right = front_right * tl + (1 - tl) * back_right
-    contour = np.concatenate([front, left, back, right], axis=0).astype(np.float32)
+    contour = tf.cast(tf.concat([front, left, back, right], axis=0), tf.float32)
